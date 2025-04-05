74eb7fd89f22ccd88631309794b2ab4e46162451
Arno <arno.pion007@gmail.com>
2025-04-05 14:55:28 +0200

    test: remove commented-out tests for Backstage Passes and Conjured items

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -94,41 +94,9 @@ describe('Gilded Rose', () => {
     expect(items[0].sellIn).toBe(0);
   });
 
-  // it('should increase Backstage Passes quality by 2 when 10 >= sellIn > 5', () => {
-  //   const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].quality).toBe(22);
-  // });
-
-  // it('should increase Backstage Passes quality by 3 when 5 >= sellIn >= 1', () => {
-  //   const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].quality).toBe(23);
-  // });
-
-  // it('should drop Backstage Passes quality to 0 after the concert', () => {
-  //   const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].quality).toBe(0);
-  // });
-
-  // it('should degrade Conjured items twice as fast before sell date', () => {
-  //   const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 3, 6)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].quality).toBe(4);
-  //   expect(items[0].sellIn).toBe(2);
-  // });
-
-  // it('should degrade Conjured items twice as fast after sell date', () => {
-  //   const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 0, 6)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].quality).toBe(2);
-  //   expect(items[0].sellIn).toBe(-1);
-  // });
-
-  // it('should not set Conjured item quality below 0', () => {
-  //   const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 0, 1)]);
-  //   const items = gildedRose.updateQuality();
-  //   expect(items[0].quality).toBe(0);
-  // });
+  it('should not set Conjured item quality below 0', () => {
+    const gildedRose = new GildedRose([new Item('Conjured Mana Cake', 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
 });
